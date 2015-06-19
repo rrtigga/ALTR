c459108ae8d4a5b2f879359d5e1e69736b6dae62
rrtigga <rrtigga@ucdavis.edu>
2015-06-19 10:32:38 -0700

    figured out how to strip

--- post.py
+++ post.py
@@ -4,6 +4,6 @@ for i, line in enumerate(input_file):
     if i == 0:
         output.write(line)
     else:
-        if not line.startswith('status'):
+        if not (line.startswith('status') or ("entries" in line) or("password" in line)) :
             output.write(line)
 
